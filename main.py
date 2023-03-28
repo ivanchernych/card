555543858e7c9c7052cdae83af767f91479cb8f3
ivan <d1zzzy.project@gmail.com>
2023-03-28 14:18:08 +0300

    task 1

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ app = flask.Flask(__name__)
 
 
 @app.route('/')
-@app.route('/list_prof/<list>')
-def training(list):
-    professions = ['инженер-исследователь', 'пилот', 'строитель', 'врач', 'штурман', 'пилот дронов']
-    return flask.render_template('base.html', list=list, prof=professions)
+@app.route('/index/<title>')
+def training(title):
+    return flask.render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
