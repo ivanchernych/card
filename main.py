f18c3392a1cd0ff2f15ddc7cc49cac61b84df898
ivan <d1zzzy.project@gmail.com>
2023-04-24 19:16:35 +0300

    ready task galery

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-import flask
+from flask import Flask, request, redirect, render_template
+import os
+from werkzeug.utils import secure_filename
 
-app = flask.Flask(__name__)
+UPLOAD_FOLDER = 'static/img'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
-@app.route('/')
-@app.route('/auto_answer')
-def auto_answer():
-    param = {
-        'title': 'Анкета',
-        'surname': 'Watny',
-        'name': 'Mark',
-        'education': 'выше среднего',
-        'profession': 'штурман марсохода',
-        'sex': 'male',
-        'motivation': 'Всегда мечтал застрять на Марсе!',
-        'ready': 'True'
-    }
-    return flask.render_template('auto_answer.html', **param)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/galery',  methods=['GET', 'POST'])
+def galery():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    list_image = os.listdir('static/img')
+    new_list_image = []
+    for el in list_image[1:]:
+        new_list_image.append(f'/static/img/{el}')
+    return render_template('galery.html', list_image=new_list_image)
 
 
 if __name__ == '__main__':
