0a186b98a364bb05a913d7798065007e9915383e
ivan <d1zzzy.project@gmail.com>
2023-04-24 20:56:38 +0300

    ready task card

--- main.py
+++ main.py
@@ -1,39 +1,18 @@
-from flask import Flask, request, redirect, render_template
-import os
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = 'static/img'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/galery',  methods=['GET', 'POST'])
-def galery():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-    list_image = os.listdir('static/img')
-    new_list_image = []
-    for el in list_image[1:]:
-        new_list_image.append(f'/static/img/{el}')
-    return render_template('galery.html', list_image=new_list_image)
+@app.route('/')
+@app.route('/member')
+def member():
+    print('+')
+    list_members = []
+    with open('templates/crew_members.json', "r", encoding='utf8') as f:
+        lst = json.loads(f.read())
+    return render_template('galery.html', lst=lst['members'])
 
 
 if __name__ == '__main__':
